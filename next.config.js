01f7ee269582403d98772f085fedc72c0e3309b5
Myt H Mon <mytmon00@gmail.com>
2026-01-13 11:25:32 +0630

    fix: Exclude frontend and backend directories from Next.js build

--- next.config.js
+++ next.config.js
@@ -4,6 +4,21 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  // Exclude frontend and backend directories from build
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  webpack: (config, { isServer }) => {
+    // Exclude frontend and backend directories
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: [
+        '**/node_modules/**',
+        '**/.next/**',
+        '**/frontend/**',
+        '**/backend/**',
+      ],
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
